04984cbf263566fffd1d42f7045a366bab231ac1
capu25 <73490102+capu25@users.noreply.github.com>
2023-02-09 15:29:07 +0100

    MainMenu + AboutPage

---
+++ app/src/main/java/sms/team/culturea/about/AboutActivity.java
@@ -0,0 +1,16 @@
+package sms.team.culturea.about;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import sms.team.culturea.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_activity);
+    }
+}
--- app/src/main/java/sms/team/culturea/menu/MenuActivity.java
+++ app/src/main/java/sms/team/culturea/menu/MenuActivity.java
@@ -6,14 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import sms.team.culturea.R;
+import sms.team.culturea.about.AboutActivity;
+import sms.team.culturea.quiz.MainQuizActivity;
 import sms.team.culturea.snake.SnakeActivity;
 import sms.team.culturea.alpaca.AlpacaActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
-    private ImageButton btnStartAlpaca, btnStartSnake;
+    private ImageView btnStartAlpaca, btnStartSnake, btnStartQuiz, btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -38,11 +41,28 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, MainQuizActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, AboutActivity.class);
+            }
+        });
     }
 
     private void initViews()
     {
         btnStartAlpaca = findViewById(R.id.btnStartAlpaca);
         btnStartSnake = findViewById(R.id.btnStartSnake);
+        btnStartQuiz = findViewById(R.id.btnStartQuiz);
+        btnAbout = findViewById(R.id.btnAbout);
     }
 }
